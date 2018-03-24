c4b6099a5a66716cb57305b5d224ffd939df82c8
Franco <fpyrih@dc.uba.ar>
2018-03-24 17:14:29 -0300

    el main levanta un archivo y lo escupe. compilé con  y corrí one two three testing hello oh mic check

--- main.cpp
+++ main.cpp
@@ -1,4 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
 int main(int argc, char** argv){
+
+	string line;
+
+	ifstream f_test("test.txt");
+	if ( f_test.is_open() ){
+		while( getline(f_test, line) ){
+			cout << line << endl;
+		}
+		f_test.close();
+	} else {
+		cout << "Unable to open file." << endl;
+	}
 
 	return 0;
 }
