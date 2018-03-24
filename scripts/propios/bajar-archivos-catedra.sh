733f81b088b8bde8d189e41d8e8709534e2e09b8
Franco <fpyrih@dc.uba.ar>
2018-03-24 17:15:20 -0300

    un script para bajar los archivos de la cátedra :P

---
+++ scripts/propios/bajar-archivos-catedra.sh
@@ -0,0 +1,5 @@
+# TODO incluir universal installer de wget https://www.xkcd.com/1654/
+wget --force-html -i http://www-2.dc.uba.ar/materias/metnum/dnload/2018_C1/tp1/tp1.html
+unzip tests_tp1.sh
+#rm tests_tp1.sh
+
