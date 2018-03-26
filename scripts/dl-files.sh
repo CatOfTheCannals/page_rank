0ae909aa246b80060522974da05c4c144a46b6bb
Franco <fpyrih@dc.uba.ar>
2018-03-26 16:37:49 -0300

    minor: translated script, fixed downloaded file decompression, changed mode to executable

---
+++ scripts/dl-files.sh
@@ -0,0 +1,5 @@
+# TODO include universal installer script for wget https://www.xkcd.com/1654/
+wget --force-html -i http://www-2.dc.uba.ar/materias/metnum/dnload/2018_C1/tp1/tp1.html
+unzip tests_tp1.zip
+#rm tests_tp1.zip
+
--- scripts/propios/bajar-archivos-catedra.sh
+++
@@ -1,5 +0,0 @@
-# TODO incluir universal installer de wget https://www.xkcd.com/1654/
-wget --force-html -i http://www-2.dc.uba.ar/materias/metnum/dnload/2018_C1/tp1/tp1.html
-unzip tests_tp1.sh
-#rm tests_tp1.sh
-
