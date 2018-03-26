0f2d045d2c7a7e7a4dd0fcdb08cd7c434cfa2542
Franco <fpyrih@dc.uba.ar>
2018-03-26 17:15:42 -0300

    minor: dl-files.sh comment to remove __MACOSX folder (sorry, I'm not touching this script anymore)

--- scripts/dl-files.sh
+++ scripts/dl-files.sh
@@ -1,5 +1,6 @@
 # TODO include universal installer script for wget https://www.xkcd.com/1654/
 wget --force-html -i http://www-2.dc.uba.ar/materias/metnum/dnload/2018_C1/tp1/tp1.html
 unzip tests_tp1.zip
+#rm -rf __MACOSX
 #rm tests_tp1.zip
 
