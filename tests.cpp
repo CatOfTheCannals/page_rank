8e867170e9d85c5e9c910af31e7e4255272d40be
ronibarylko <ronibarylko@hotmail.com>
2018-03-29 16:48:59 -0300

    Soy carlos xd xd

---
+++ tests.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include "gtest/gtest.h"
+
+// --------- SET UP --------------
+class runTest : public ::testing::Test {
+protected:
+    virtual void SetUp() {
+        int i = 0;
+        while (i < 10){
+            int j = 0;
+            while (j < 10) {
+                bigArray[i][j] = rand() % 100;//Array con elementos random
+                j++;
+            }
+            i++;
+        }
+        i = 0;
+        while (i < 1000){
+            reallyBig[i] = rand() % 200;
+            i++;
+        }
+    }
+    int bigArray[10][10]; //100 arrays de 10 elementos random
+    int reallyBig[1000];
+};
+
+TEST_F (runTest, nombreTest){
+    int i = 0;
+    while (i < 10) {
+        ASSERT_EQ(true, true);
+        std::cout << "Corrida numero " << i << std::endl;
+        i++;
+    }
+}
