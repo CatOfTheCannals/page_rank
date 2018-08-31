718ae16e59ec4c513a6db500a5faef7af21f0bde
lstrika <lstrika@ws22.labo6>
2018-08-31 19:34:26 -0300

    mejorar interfaz de generador

--- test/generateRandomMatrix.py
+++ test/generateRandomMatrix.py
@@ -1,6 +1,8 @@
 import random
 import sys
 def generateMatrix(dim, qty, name, g = lambda x, y : x != y ):
+    if qty > dim*dim:
+        raise ValueError('invalid input. More links than possible for the supplied matrix.')
     s = str(dim)+"\n"+str(qty)+"\n"
     pairs = [(i,j) for i in range(1,dim+1) for j in range(1,dim+1) if g(i,j)]
     used_pairs = random.sample(pairs, qty)
@@ -8,8 +10,14 @@ def generateMatrix(dim, qty, name, g = lambda x, y : x != y ):
 	s+= str(p)+" "+str(q)+"\n"
     with open(name, 'w') as f:
 	    f.write(s)
+
 args = sys.argv[1:]
 #dim, qty, name
-generateMatrix(int(args[0]),int(args[1]),args[2])	
 
+def main(args):
+    try:
+        generateMatrix(int(args[0]),int(args[1]),args[2])	
+    except:
+        raise ValueError('invalid input. terminating. Correct usage should be: generateMatrix.py <DIMENSION> <QUANTITY OF LINKS> <OUTPUT FILE NAME>.')
 
+main(args)
